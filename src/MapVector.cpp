0826daddb646a7b51e730b64feea8d8ba05230e0
C4ntasaur <157429343+C4ntsaur@users.noreply.github.com>
2025-03-12 19:06:15 +0000

    Updated private member variables to '_' prefix. Added comments.

--- src/MapVector.cpp
+++ src/MapVector.cpp
@@ -1,167 +1,148 @@
-#include "Map.h"
+#include "MapVector.h"
 #include <fstream>
 #include <sstream>
 #include <iostream>
 #include <type_traits>
 
-// Constructors
+/**
+ * @brief Construct a new Map<std::vector< T>>:: Map object
+ * @tparam T Numeric type
+ */
 template <typename T>
 Map<std::vector<T>>::Map() {
-    width = 0;
-    height = 0;
+    _width = 0;
+    _height = 0;
 }
 
+/**
+ * @brief Construct a new Map<std::vector< T>>:: Map object of size _width, _height
+ */
 template <typename T>
-Map<std::vector<T>>::Map(int w, int h) : width(w), height(h) {
+Map<std::vector<T>>::Map(int w, int h) : _width(w), _height(h) {
     // Sets all positions to default T
     mapData.resize(height);
-
-    for (int i = 0; i < height; i++) {
-        mapData.resize(width);
+    for (int i = 0; i < _height; i++) {
+        _mapData.resize(_width);
     }
 }
 
-// Public Methods
+/**
+ * @brief Delegation method for loading 2D array of vector data
+ */
 template <typename T>
 bool Map<std::vector<T>>::loadFromFile(const std::string& filename, const std::string& format) {
     if (format == "txt") {
         return loadFromTXT(filename);
     }
-    else if (format == "bin") {
-        return loadFromBin(filename);
-    }
     else {
         std::cerr << "Unsupported file format: " << format << std::endl;
         return false;
     }
 }
 
+/**
+ * @brief Delegation method for saving 2D array of vector data
+ */
 template <typename T>
 bool Map<std::vector<T>>::saveToFile(const std::string& filename, const std::string& format) const {
     if (format == "txt") {
         return saveToTXT(filename);
     }
-    else if (format == "bin") {
-        return saveToBin(filename);
-    }
     else {
         std::cerr << "Unsupported file format: " << format << std::endl;
         return false;
     }
 }
 
+/**
+ * @brief Retrieve vector from _mapdata
+ */
 template <typename T>
 std::vector<T>& Map<std::vector<T>>::getData(int x, int y) {
-    return mapData[x][y];
+    return _mapData[y][x];
 }
 
+/**
+ * @brief Set vector in _mapData
+ */
 template <typename T>
 void Map<std::vector<T>>::setData(int x, int y, const std::vector<T>& value) {
-    mapData[x][y] = value;
+    _mapData[y][x] = value;
 }
 
+/**
+ * @brief Return _width
+ */
 template <typename T>
 int Map<std::vector<T>>::getWidth(void) const{
-    return width;
+    return _width;
 }
 
+/**
+ * @brief Return _height
+ */
 template <typename T>
 int Map<std::vector<T>>::getHeight(void) const{
-    return height;
+    return _height;
 }
 
-//Private Methods
-
-// General int or float use for .txt
+/**
+ * @brief Load 2D array of vector data from .txt file
+ */
 template <typename T>
 bool Map<std::vector<T>>::loadFromTXT(const std::string& filename) {
     std::ifstream file(filename);
     std::string line;
 
+    // Check correct opening
     if (!file.is_open()) {
         std::cerr << "Failed to open file: " << filename << std::endl;
         return false;
     }
 
+    // Iterate over file to get data
     while (std::getline(file, line)) {
         std::stringstream ss(line);
         std::string token;
 
         std::vector<std::vector<T>> rowData;
+        // Split by space to get individual vectors
         while (std::getline(ss, token, ' ')) {
             std::stringstream tokenStream(token);
             std::vector<T> vecData;
             
-            // Read comma-separated data inside the vector
+            // Read comma separated data inside the vector
             while (std::getline(tokenStream, token, ',')) {
                 std::stringstream valueStream(token);
                 T value;
                 valueStream >> value;
                 vecData.push_back(value);
             }
-
             rowData.push_back(vecData);
         }
-
-        mapData.push_back(rowData);
+        _mapData.push_back(rowData);
     }
-
-    height = mapData.size();
-    if (height > 0) {
-        width = mapData[0].size();
+    _height = _mapData.size();
+    if (_height > 0) {
+        _width = _mapData[0].size();
     }
-
     return true;
 }
 
-
-template <typename T>
-bool Map<std::vector<T>>::loadFromBin(const std::string& filename) {
-    std::ifstream file(filename, std::ios::binary);
-
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file: " << filename << std::endl;
-        return false;
-    }
-
-    // Get integer start values: height, width, and vector size
-    file.read(reinterpret_cast<char*>(&height), sizeof(height));
-    file.read(reinterpret_cast<char*>(&width), sizeof(width));
-
-    int vectorSize = 0;
-    file.read(reinterpret_cast<char*>(&vectorSize), sizeof(vectorSize));
-
-    if (height <= 0 || width <= 0 || vectorSize <= 0) {
-        std::cerr << "Invalid (height, width, or vector size) from the binary file." << std::endl;
-        return false;
-    }
-
-    
-    mapData.resize(height);
-
-    
-    for (int i = 0; i < height; i++) {
-        mapData.resize(width);
-        for (int j = 0; j < width; j++) {
-            mapData[i][j].resize(vectorSize);
-            file.read(reinterpret_cast<char*>(mapData[i][j].data()), vectorSize * sizeof(T));
-        }
-    }
-
-    return true;
-}
-
-
+/**
+ * @brief Save 2D array of vector data to .txt file
+ */
 template <typename T>
 bool Map<std::vector<T>>::saveToTXT(const std::string& filename) const {
     std::ofstream file(filename);
+    // Check opening of file
     if (!file.is_open()) {
         std::cerr << "Failed to open file for writing: " << filename << std::endl;
         return false;
     }
 
     // Per row write values with space separation
-    for (const auto& row : mapData) {
+    for (const auto& row : _mapData) {
         bool firstVector = true;
         
         for (const auto& vec : row) {
@@ -169,42 +150,16 @@ bool Map<std::vector<T>>::saveToTXT(const std::string& filename) const {
                 file << " ";
             }
             firstVector = false;
-        
+            
+            // Per vector write values with comma separation
             for (size_t i = 0; i < vec.size(); i++) {
                 file << vec[i];
                 if (i < vec.size() - 1) {
                     file << ",";
                 }
             }
-        
         }
     file << std::endl;
-    
-    }
-    return true;
-}
-
-template <typename T>
-bool Map<std::vector<T>>::saveToBin(const std::string& filename) const {
-    std::ofstream file(filename, std::ios::binary);
-
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file for writing: " << filename << std::endl;
-        return false;
-    }
-
-    // Write integer height, width, and internal vector size
-    file.write(reinterpret_cast<const char*>(&height), sizeof(height));
-    file.write(reinterpret_cast<const char*>(&width), sizeof(width));
-
-    int vectorSize = mapData[0][0].size();
-    file.write(reinterpret_cast<const char*>(&vectorSize), sizeof(vectorSize));
-
-    // Per row write values
-    for (const auto& row : mapData) {
-        for (const auto& vec : row) {
-            file.write(reinterpret_cast<const char*>(vec.data()), vectorSize * sizeof(T));
-        }
     }
     return true;
 }
