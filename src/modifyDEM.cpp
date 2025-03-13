198a16467f90a1753a1b4f9e529ecfbd7d036a74
C4ntasaur <157429343+C4ntsaur@users.noreply.github.com>
2025-03-13 19:04:04 +0000

    Added comments. Updated renamed variables from Map.h refactor

--- src/modifyDEM.cpp
+++ src/modifyDEM.cpp
@@ -1,39 +1,49 @@
+/**
+ * @file modifyDEM.cpp
+ * @author Ollie
+ * @brief Methods to clean DEM data
+ * @version 1.0.0
+ * @date 2025-03-13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #include <limits>
-#include <algorithm>  // For std::min and std::max
+#include <algorithm>
 #include "Map.h"
 
+/**
+ * @brief Method to remove sinks from DEM data
+ */
 template <typename T>
 void Map<T>::fillSinks(void) {
     int dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
     int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
-    int width = getWidth();
-    int height = getHeight();
-
     bool modified;
     do {
         modified = false;
-        for (int y = 1; y < height - 1; y++) {
-            for (int x = 1; x < width - 1; x++) {
+        for (int y = 1; y < _height - 1; y++) {
+            for (int x = 1; x < _width - 1; x++) {
                 if (is_sink(x, y)) {
-                    // Find the lowest neighbor elevation to fill the sink
+                    // Find the lowest neighbour elevation to fill the sink
                     T min_neighbor_value = std::numeric_limits<T>::infinity();
                     bool has_lower_neighbor = false;
                     for (int direction = 0; direction < 8; direction++) {
                         int nx = x + dx[direction];
                         int ny = y + dy[direction];
-                        if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
-                            // Only consider neighbors with non-zero values (skip lakes or low-lying areas)
-                            if (mapData[ny][x] > 0) {
-                                min_neighbor_value = std::min(min_neighbor_value, mapData[ny][nx]);
+                        if (nx >= 0 && nx < _width && ny >= 0 && ny < _height) {
+                            // Consider values greater than 0
+                            if (_mapData[ny][x] > 0) {
+                                min_neighbor_value = std::min(min_neighbor_value, _mapData[ny][nx]);
                                 has_lower_neighbor = true;
                             }
                         }
                     }
 
-                    // If there's a valid lower neighbor, raise the current cell to just above that neighbor
-                    if (has_lower_neighbor && mapData[y][x] < min_neighbor_value) {
-                        mapData[y][x] = min_neighbor_value + 1;  // Raise slightly above the lowest neighbor
+                    // Raise sink cell to slightly greater than lowest neighbour
+                    if (has_lower_neighbor && _mapData[y][x] < min_neighbor_value) {
+                        _mapData[y][x] = min_neighbor_value + 1;
                         modified = true;
                     }
                 }
@@ -42,21 +52,24 @@ void Map<T>::fillSinks(void) {
     } while (modified);
 }
 
+
+/**
+ * @brief Bool check for if cell at (x, y) is a sink.
+ * Sink = all neighbours are greater
+ */
 template <typename T>
 bool Map<T>::is_sink(int x, int y) {
     int dx[] = {1, 1, 0, -1, -1, -1, 0, 1};
     int dy[] = {0, 1, 1, 1, 0, -1, -1, -1};
     
-    T current_value = mapData[y][x];
-    int width = getWidth();
-    int height = getHeight();
-
+    T current_value = _mapData[y][x];
+    
     for (int direction = 0; direction < 8; direction++) {
         int nx = x + dx[direction];
         int ny = y + dy[direction];
-        if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+        if (nx >= 0 && nx < _width && ny >= 0 && ny < _height) {
             // If there is a neighboring cell with lower elevation, it's not a sink
-            if (mapData[ny][nx] < current_value) {
+            if (_mapData[ny][nx] < current_value) {
                 return false;
             }
         }
